e87234263fef8419173be1c572b87f1d1de04ee2
Amith <badesha_a07@hotmail.com>
2019-10-28 19:53:56 -0700

    commit

--- COMP4711/Lab 3 V2/script.js
+++ COMP4711/Lab 3 V2/script.js
@@ -10,31 +10,31 @@ function showForm() {
 }
 var counter = 0;
 function addToDirectory() {
-  var artistName = document.getElementById("artistName").value;
-  var aboutArtist = document.getElementById("aboutArist").value;
-  var imageURL = document.getElementById("imgURL").value;
+  var artistName = document.getElementByName("artistName").value;
+  var aboutArtist = document.getElementByName("aboutArist").value;
+  var imageURL = document.getElementByName("imgURL").value;
 
   var div = document.createElement('div');
-  div.setAttribute('class', 'content');
-  div.setAttribute('id', counter);
+  div.setAttribute('id', 'content');
+  div.setAttribute('name', counter);
   document.body.appendChild(div);
 
-  var x = document.createElement("IMG");
+  var x = document.createElement("img");
   x.setAttribute("src", imageURL);
   div.appendChild(x);
 
   var div2 = document.createElement('div');
-  div2.setAttribute('class', 'artistName');
+  div2.setAttribute('id', 'artistName');
   div2.textContent = artistName;
   div.appendChild(div2);
 
   var div3 = document.createElement('div');
-  div3.setAttribute('class', 'artistSchool');
+  div3.setAttribute('id', 'artistSchool');
   div3.textContent = aboutArtist;
   div2.appendChild(div3);
 
   var btn = document.createElement("button");
-  btn.setAttribute('class', 'deleteBtn');
+  btn.setAttribute('id', 'deleteBtn');
   btn.setAttribute('onclick', 'deleteDiv(' + counter + ')');
   btn.innerHTML = 'Delete';
   div3.appendChild(btn);
